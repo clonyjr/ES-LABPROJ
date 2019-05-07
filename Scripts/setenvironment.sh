cc491f7ff4502269af1edbff9496ab09b3b9c420
clonyjr <clony.nunes@gmail.com>
2019-05-07 11:22:39 +0100

    ajuste kafka

---
+++ Scripts/setenvironment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo creating environment variables
+echo ..
+
+echo 'export AIRLOCATION_URL="https://opensky-network.org/api/states/all"'>>~/.bash_profile
+echo 'export AIRLOCATION_LAMIN="45.8389"'>>~/.bash_profile
+echo 'export AIRLOCATION_LOMIN="5.9962"'>>~/.bash_profile
+echo 'export AIRLOCATION_LAMAX="47.8229"'>>~/.bash_profile
+echo 'export AIRLOCATION_LOMAX="10.5226"'>>~/.bash_profile
